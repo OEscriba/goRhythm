7b471ca459c3d63084c8bab264c63e874d9749dc
Almeedus <eduardo.almeida26@fatec.sp.gov.br>
2021-06-17 07:35:05 -0300

    adiciona players

---
+++ JAZZ/data.js
@@ -0,0 +1,32 @@
+window.audios = [
+  {
+    title: "Havona",
+    artist: "Weather Report",
+    cover: "background.jpeg",
+    file: "Havona.mp3"
+  },
+  {
+    title: "Spain",
+    artist: "Chick Corea",
+    cover: "background.jpeg",
+    file: "Spain.mp3"
+  },
+  {
+    title: "Spokes",
+    artist: "Allan Holdsworth",
+    cover: "background.jpeg",
+    file: "Spokes.mp3"
+  },
+  {
+    title: "So what",
+    artist: "Miles Davis ",
+    cover: "background.jpeg",
+    file: "So-What.mp3"
+  },
+  {
+    title: "Birdland",
+    artist: "Weather Report",
+    cover: "background.jpeg",
+    file: "Birdland.mp3"
+  }
+];
---
+++ ROCK/data.js
@@ -0,0 +1,32 @@
+window.audios = [
+  {
+    title: "YYZ",
+    artist: "Rush",
+    cover: "background.jpeg",
+    file: "YYZ.mp3"
+  },
+  {
+    title: "Oscilate Wild",
+    artist: "The smiths",
+    cover: "background.jpeg",
+    file: "Oscilate-Wild.mp3"
+  },
+  {
+    title: "Scene Seven: I. The Dance of Eternity",
+    artist: "Dream Theater",
+    cover: "background.jpeg",
+    file: "Dance-of-Eternity.mp3"
+  },
+  {
+    title: "Voice of the Soul",
+    artist: "Death",
+    cover: "background.jpeg",
+    file: "Voice-of-the-Soul.mp3"
+  },
+  {
+    title: "Moby Dick",
+    artist: "Led Zeppelin",
+    cover: "background.jpeg",
+    file: "Moby-Dick.mp3"
+  }
+];
